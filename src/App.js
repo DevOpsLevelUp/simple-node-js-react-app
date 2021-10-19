77a5c4423282f5991a9a6dfbffa9d06b2f8e91fb
tekup2021 <90339234+tekup2021@users.noreply.github.com>
2021-10-19 19:24:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <h1>This Application Demo_V1 is deployed using jenkins pipeline </h1>
+          <h1>This Application Demo_V2 is deployed using jenkins pipeline </h1>
         <img src="https://d1q6f0aelx0por.cloudfront.net/product-logos/f5326186-8ae7-425c-a78d-7192dabf75be-jenkins.png"  />
       </div>
     );
